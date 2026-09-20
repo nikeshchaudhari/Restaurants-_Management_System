86743480698444abcd0eefb2fd1edfd2845ce5c8
Nikesh <info.nikesh12@gmail.com>
2026-09-20 15:24:00 +0545

    Login

--- app/api/users/login/route.ts
+++ app/api/users/login/route.ts
@@ -1,26 +1,72 @@
+import { NextResponse } from "next/server";
 import db from "@/lib/db";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-export async function POST(req: Request) {
+export const POST = async (req: Request) => {
   try {
-    const { email, password } = await req.json();
+    const body = await req.json();
+    const username = body.username;
+    const password = body.password;
 
-    const [users] = await db.query(
-      "SELECT * FROM users WHERE email = ? AND password = ?",
-      [email, password]
-    );
+    const query =
+      "SELECT * FROM tblusers WHERE username = ? LIMIT 1";
 
-    if ((users as any[]).length === 0) {
-      return new Response("Invalid email or password", {
-        status: 401,
-      });
+    const findUser = await db.query(query, [username,password]);
+
+    console.log("DB RESULT:", findUser);
+
+    if (Array.isArray(findUser[0]) && findUser[0].length === 0) {
+      return NextResponse.json(
+        {
+          msg: "Invalid username or password",
+        },
+        {
+          status: 401,
+        }
+      );
     }
 
-    return new Response("Login successful");
-  } catch (error) {
-    console.error(error);
+    const user = findUser[0];
 
-    return new Response("Login failed", {
-      status: 500,
+    console.log("USER:", user);
+
+    return NextResponse.json({
+      msg: "User found",
     });
+
+
+    // match password
+    
+    const isMatch = await bcrypt.compare(password,user.password_hash);
+
+    if(!isMatch){
+      return NextResponse.json(
+         {
+          msg: "Invalid username or password",
+        },
+        {
+          status: 401,
+        }
+      )
+    }
+
+    
+
+
+   
+
+  } catch (error) {
+    console.error("LOGIN ERROR:", error);
+
+    return NextResponse.json(
+      {
+        msg: "Internal Server Error",
+        error: String(error),
+      },
+      {
+        status: 500,
+      }
+    );
   }
-}
+};
