81f42048561086de4f53d2ceb670eaddbb794b83
Nikesh <info.nikesh12@gmail.com>
2026-09-21 14:26:10 +0545

    middleware auth

---
+++ middelware.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+export const middlware = async (req: NextRequest) => {
+  const token = req.cookies.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.json({ msg: "Unauthorized" }, { status: 401 });
+  }
+
+  try {
+    const verifyToken = (await jwt.verify(token, process.env.JWT_SECRET!)) as {
+      userId: number;
+      roleId: number;
+      restaurantId: number | null;
+    };
+
+    // only superAdmin
+
+    if (verifyToken.roleId !== 1) {
+      return NextResponse.json(
+        { msg: "Only Super Admin can access this" },
+        { status: 403 },
+      );
+    }
+
+    console.log(verifyToken);
+
+    return NextResponse.next();
+  } catch (err) {
+    console.log("Error");
+
+    return NextResponse.json(
+      {
+        error: err,
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+};
+
+export const config = {
+  matcher: ["/api/users/superadmin/:path*"],
+};
