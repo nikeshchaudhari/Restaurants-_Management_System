7a8570ea85a92013985d2d6ed8e43f28935313ad
Nikesh <info.nikesh12@gmail.com>
2026-09-21 17:44:32 +0545

    all

--- app/api/users/login/route.ts
+++ app/api/users/login/route.ts
@@ -1,26 +1,108 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import db from "@/lib/db";
 
-export async function POST(req: Request) {
+export const POST = async (req: Request) => {
   try {
-    const { email, password } = await req.json();
+    const body = await req.json();
 
-    const [users] = await db.query(
-      "SELECT * FROM users WHERE email = ? AND password = ?",
-      [email, password]
+    // Check username and password
+    const { username, password } = body;
+
+    //  Check username
+    const checkQuery =
+      "SELECT * FROM tblusers WHERE username = ? LIMIT 1";
+
+    const [users]: any = await db.query(
+      checkQuery,
+      [username]
     );
 
-    if ((users as any[]).length === 0) {
-      return new Response("Invalid email or password", {
-        status: 401,
-      });
+    // Username not found
+    if (users.length === 0) {
+      return NextResponse.json(
+        {
+          msg: "Invalid username or password",
+        },
+        {
+          status: 401,
+        }
+      );
     }
 
-    return new Response("Login successful");
-  } catch (error) {
-    console.error(error);
+    // Get user
+    const user = users[0];
 
-    return new Response("Login failed", {
-      status: 500,
+    //  Check password
+    const passwordMatch = await bcrypt.compare(
+      password,
+      user.password_hash
+    );
+
+    // Password incorrect
+    if (!passwordMatch) {
+      return NextResponse.json(
+        {
+          msg: "Invalid username or password",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    //  Create JWT token
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        roleId: user.role_id,
+        restaurantId: user.restaurant_id,
+      },
+      process.env.JWT_SECRET!,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    // Create response
+    const response = NextResponse.json(
+      {
+        msg: "Login successful",
+        user: {
+          id: user.id,
+          name: user.name,
+          username: user.username,
+          role_id: user.role_id,
+          restaurant_id: user.restaurant_id,
+        },
+      },
+      {
+        status: 200,
+      }
+    );
+
+    // Store token in cookie
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 60 * 60 * 24*7,
+      path: "/",
     });
+
+    return response;
+
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        msg: "Something went wrong",
+      },
+      {
+        status: 500,
+      }
+    );
   }
-}
+};
