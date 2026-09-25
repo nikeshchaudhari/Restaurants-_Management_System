5de077f21881550638cdea82473dcc9a30499290
Nikesh <info.nikesh12@gmail.com>
2026-09-25 13:11:52 +0545

    superadmin update data

--- app/api/users/superadmin/[id]/route.ts
+++ app/api/users/superadmin/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import db from "@/lib/db";
 import { RowDataPacket } from "mysql2";
-
+import bcrypt from "bcryptjs";
 interface superAdminData extends RowDataPacket {
   id: number;
   role_id: number;
@@ -39,13 +39,13 @@ export const DELETE = async (req: NextRequest) => {
 
     const deleteQuery = `DELETE from tblusers WHERE id =? AND role_id =1`;
 
-    await db.query(deleteQuery,[id]);
+    await db.query(deleteQuery, [id]);
     return NextResponse.json(
-        {msg: "Superadmin deleted successfully"},
-        {
-             status: 200 
-        }
-    )
+      { msg: "Superadmin deleted successfully" },
+      {
+        status: 200,
+      },
+    );
 
     return NextResponse.json(
       { msg: "SuperAdmin deleted successfully" },
@@ -53,6 +53,54 @@ export const DELETE = async (req: NextRequest) => {
     );
   } catch (err) {
     console.log("error");
+    return NextResponse.json(
+      {
+        error: err,
+      },
+      { status: 500 },
+    );
+  }
+};
+
+// Update superadmin
+
+export const PUT = async (req: NextRequest) => {
+  try {
+    const url = await new URL(req.url);
+    const id = url.pathname.split("/").pop();
+    console.log(id);
+
+    const body = await req.json();
+    const { name, username, phone, password, status } = body;
+
+    // check superadmin
+    const checkQuery = `SELECT id FROM tblusers WHERE id=? AND role_id=1 LIMIT 1`;
+    const superAdminData = await db.query<superAdminData[]>(checkQuery, [id]);
+    const data = superAdminData[0];
+    console.log(superAdminData);
+    
+
+    if (data.length === 0) {
+      return NextResponse.json(
+        { msg: "Superadmin not found" },
+        { status: 404 },
+      );
+    }
+
+
+    // password hash
+
+    const hashPassword = await bcrypt.hash(password,10);
+    const updateQuery = `UPDATE tblusers SET name=?, username=?, password_hash=?, phone=?, status=? WHERE id=? AND role_id=1`;
+
+    await db.query(updateQuery, [name, username, hashPassword, phone,status,id]);
+
+    return NextResponse.json({
+      msg: "Superadmin updated successfully",
+    });
+  } catch (err) {
+    console.log("error");
+
     return NextResponse.json(
       {
         error: err,
